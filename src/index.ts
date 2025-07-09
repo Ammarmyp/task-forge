3c35e97728475b3a411f086ceb452da1084cb9d2
Ammarmyp <ammarmyp@gmail.com>
2025-07-09 12:38:10 +0300

    add cors on the start of the application

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { serve } from "@hono/node-server";
 import "dotenv/config";
 import Routes from "./routes";
 
 const app = new Hono();
+
+app.use("/*", cors());
 
 app.get("/", (c) => {
   return c.text("Hello Hono! task forge running using it porperly");
